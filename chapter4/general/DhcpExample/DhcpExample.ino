af6bb9aca3c08a22a3fb826efb7ce905bd433aa4
Tom Igoe <tom.igoe@gmail.com>
2011-05-31 21:09:55 -0400

    Updated DHCP example

--- chapter4/general/DhcpExample/DhcpExample.ino
+++ chapter4/general/DhcpExample/DhcpExample.ino
@@ -1,10 +1,12 @@
 /* DHCP
-   Language: Arduino
-*/
+ Language: Arduino
+ */
 #include <SPI.h>
 #include <Ethernet.h>
 
-byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01};
+byte mac[] = { 
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01};
+IPAddress ip(192,168,1,20);            // an address to use if DHCP fails
 
 void setup() {
   // start the serial library:
@@ -12,24 +14,23 @@ void setup() {
   // start the Ethernet connection:
   Serial.println("Asking for an IP address using DHCP...");
   if (!Ethernet.begin(mac)) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    //You didn't get an address, so stop here:
-    while(true);
+    // if DHCP fails, set your own address:
+    Ethernet.begin(mac, ip);
   } 
-  else {
-    // print the bytes of the IP address, separated by dots:
-    IPAddress myIp = Ethernet.localIP();
-    Serial.print("I got an IP address. It's ");
-    Serial.print(myIp[0], DEC); 
-    Serial.print(".");
-    Serial.print(myIp[1], DEC); 
-    Serial.print(".");
-    Serial.print(myIp[2], DEC); 
-    Serial.print(".");
-    Serial.println(myIp[3], DEC); 
-  }
+  // print the bytes of the IP address, separated by dots:
+  IPAddress myIp = Ethernet.localIP();
+  Serial.print("I got an IP address. It's ");
+  Serial.print(myIp[0], DEC); 
+  Serial.print(".");
+  Serial.print(myIp[1], DEC); 
+  Serial.print(".");
+  Serial.print(myIp[2], DEC); 
+  Serial.print(".");
+  Serial.println(myIp[3], DEC); 
 }
 
 void loop() {
 }
+
+
 
