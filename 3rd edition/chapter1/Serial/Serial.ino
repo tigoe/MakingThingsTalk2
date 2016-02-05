1e1c736bd2a272389e74b35aece9934bec935ffd
tigoe <tom.igoe@gmail.com>
2016-02-05 17:14:23 -0500

    Arduino simple serial chapter 1

---
+++ 3rd edition/chapter1/Serial/Serial.ino
@@ -0,0 +1,36 @@
+/*
+    Simple Serial  
+    Context: Arduino
+    Listens for an incoming serial byte, adds one to the byte
+    and sends the result back out serially.
+    Also blinks the built-in LED every half second.
+ */
+
+int inByte = 0;              // variable to hold incoming serial data
+long blinkTimer = 0;         // keeps track of how long since the LED 
+                             // was last turned off
+int blinkInterval = 1000;    // a full second from on to off to on again
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);    // set pin 13 to be an output
+  Serial.begin(9600);         // configure the serial port for 9600 bps 
+}
+
+void loop() {
+  // if there are any incoming serial bytes available to read:
+  if (Serial.available() > 0) {
+        inByte = Serial.read();  // then read the first available byte,
+        Serial.write(inByte+1);  // add one to it and send the result out
+  }
+  
+  // Meanwhile, keep blinking the LED.
+  // after a half a second, turn the LED on:
+  if (millis() - blinkTimer >= blinkInterval / 2) {
+    digitalWrite(LED_BUILTIN, HIGH);      // turn the LED on 
+  }
+  // after a half a second, turn the LED off and reset the timer:
+  if (millis() - blinkTimer >= blinkInterval) {
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
+    blinkTimer = millis();           // reset the timer
+  }
+}
