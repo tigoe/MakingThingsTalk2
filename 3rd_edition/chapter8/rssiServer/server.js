95ac5007122198216d336c09f46bbdb47810409b
tigoe <tom.igoe@gmail.com>
2016-11-03 18:43:21 -0400

    updated chapter 8 rssiServer

--- 3rd_edition/chapter8/rssiServer/server.js
+++ 3rd_edition/chapter8/rssiServer/server.js
@@ -1,16 +1,22 @@
-
 /*
   RSSI server
   context: node.js
 */
-var express = require('express');	// include the express library
-var server = express();					  // create a server using express
+var express = require('express'); // include the express library
+var server = express();           // create a server using express
 var rssi = -100;                  // the latest received signal strength
 
+function postRssi(request, response) {
+  rssi = request.params.rssi;   // get the signal strength from the request
+  response.send("rssi received: " + rssi);  // respond to the client
+  response.end();               // close the connection
+}
 // server response to GET /rssi
 function getRssi(request, response) {
   // make a string to send to the client:
-  var message = "RSSI: " + rssi + " dBm<br>";
+  // auto-refresh meta tag:
+  var message = "<meta http-equiv=\"refresh\" content=\"3\">\n";
+  message += "Last RSSI received: " + rssi + " dBm<br>";
   message += "The receiver is ";
   if (rssi <= -60 ) {         // weakest signal strength
     message += "far away ";
@@ -32,14 +38,9 @@ function getRssi(request, response) {
   response.end();                 // close the connection
 }
 
-function setRssi(request, response) {
-  rssi = request.params.rssi;   // get the signal strength from the request
-  response.send("rssi received: " + rssi);  // respond to the client
-  response.end();                 // close the connection
-}
 // start the server:
 server.listen(8080);
 
 // define what to do when the client requests something:
 server.get('/rssi', getRssi);         // GET /rssi
-server.post('/rssi/:rssi', setRssi);  // POST /rssi/value
+server.post('/rssi/:rssi', postRssi);  // POST /rssi/value
