264eceef9cc617a56ae23011b4d3685f97040bfe
tigoe <tom.igoe@gmail.com>
2016-12-01 17:46:04 -0500

    update https server

--- 3rd_edition/chapter8/GeolocationServer/https-server.js
+++ 3rd_edition/chapter8/GeolocationServer/https-server.js
@@ -4,6 +4,8 @@ context: node.js
 
 / to create keys for self-signing: https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
 
+openssl req -newkey rsa:2048 -nodes -keyout domain.key -x509 -days 365 -out domain.crt
+
 to properly sign:
 https://certbot.eff.org
 
@@ -15,18 +17,23 @@ var http = require('http');       // require the HTTP library
 var fs = require('fs');           // require the filesystem library
 var server = express();					  // create a server using express
 
-var options = {                             // options for the HTTPS server
-  key: fs.readFileSync('./keys/domain.key'),   // the key
+var options = {                              // options for the HTTPS server
+  key: fs.readFileSync('./keys/domain.key'), // the key
   cert: fs.readFileSync('./keys/domain.crt') // the certificate
 };
 
+server.use(httpRedirect);                   // set a redirect function for http
 server.use('/',express.static('public'));   // set a static file directory
 
-function redirect(request,response){
-    response.redirect('https://' + request.hostname + request.url)
+function httpRedirect(request,response, next) {
+  if (!request.secure) {
+    console.log("redirecting http request to https");
+    response.redirect('https://' + request.hostname + request.url);
+  } else {
+    next();     // pass the request on to the express.static middleware
+  }
 }
 
 // start the server:
-https.createServer(options, server).listen(443);  // listen for HTTPS
 http.createServer(server).listen(8080);           // listen for HTTP
-http.get('*', redirect);
+https.createServer(options, server).listen(443);  // listen for HTTPS
