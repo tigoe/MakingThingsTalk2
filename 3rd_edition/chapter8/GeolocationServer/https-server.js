e6b07b0ce30024f517b0a7df71ebd0bba56af273
tigoe <tom.igoe@gmail.com>
2016-12-01 17:09:16 -0500

    https server update

--- 3rd_edition/chapter8/GeolocationServer/https-server.js
+++ 3rd_edition/chapter8/GeolocationServer/https-server.js
@@ -1,6 +1,12 @@
 /*
 HTTP/HTTPS server
 context: node.js
+
+/ to create keys for self-signing: https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
+
+to properly sign:
+https://certbot.eff.org
+
 */
 // include libraries and declare global variables:
 var express = require('express');	// include the express library
@@ -10,13 +16,17 @@ var fs = require('fs');           // require the filesystem library
 var server = express();					  // create a server using express
 
 var options = {                             // options for the HTTPS server
-  key: fs.readFileSync('./keys/key.pem'),   // the key
-  cert: fs.readFileSync('./keys/cert.pem'), // the certificate
-  passphrase: 's33kr!+'                    // the passphrase in the key
+  key: fs.readFileSync('./keys/domain.key'),   // the key
+  cert: fs.readFileSync('./keys/domain.crt') // the certificate
 };
 
 server.use('/',express.static('public'));   // set a static file directory
 
+function redirect(request,response){
+    response.redirect('https://' + request.hostname + request.url)
+}
+
 // start the server:
-http.createServer(server).listen(8080);           // listen for HTTP
 https.createServer(options, server).listen(443);  // listen for HTTPS
+http.createServer(server).listen(8080);           // listen for HTTP
+http.get('*', redirect);
