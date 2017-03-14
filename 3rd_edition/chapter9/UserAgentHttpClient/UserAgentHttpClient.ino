943beaec42613201172b460f765daf566d18c0cc
tigoe <tom.igoe@gmail.com>
2017-03-14 12:28:39 -0400

    update

---
+++ 3rd_edition/chapter9/UserAgentHttpClient/UserAgentHttpClient.ino
@@ -0,0 +1,50 @@
+/*
+  Test HTTP Client
+  Context: Arduino, with WINC1500 module
+*/
+// include required libraries and config files
+#include <SPI.h>
+#include <WiFi101.h>
+//#include <ESP8266WiFi.h>    // use this instead of WiFi101 for ESP8266 modules
+#include <ArduinoHttpClient.h>
+#include "config.h"
+
+WiFiClient netSocket;               // network socket to server
+const char server[] = "104.236.102.241";  // server name
+String route = "/";              // API route
+
+void setup() {
+  Serial.begin(9600);               // initialize serial communication
+
+  // while you're not connected to a WiFi AP,
+  while ( WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);           // print the network name (SSID)
+    WiFi.begin(ssid, pass);         // try to connect
+    delay(2000);
+  }
+
+  // When you're connected, print out the device's network status:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
+void loop() {
+  HttpClient http(netSocket, server, 8080);      // make an HTTP client
+  http.beginRequest();
+  http.get(route);  // make a GET request
+  http.sendHeader("user-agent","arduino");
+  http.endRequest();
+
+  while (http.connected()) {       // while connected to the server,
+    if (http.available()) {        // if there is a response from the server,
+      String result = http.readString();  // read it
+      Serial.print(result);               // and print it
+    }
+  }
+  // when there's nothing left to the response,
+  http.stop();                     // close the request
+  delay(10000);                    // wait 10 seconds
+}
+
