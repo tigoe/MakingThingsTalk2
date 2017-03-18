f9a7c6281fd18903534d8d23283ee4c4dccf4ec3
tigoe <tom.igoe@gmail.com>
2017-03-18 16:37:12 -0400

    added body parser

--- 3rd_edition/chapter10/catCamDeux/httpServer.js
+++ 3rd_edition/chapter10/catCamDeux/httpServer.js
@@ -5,20 +5,22 @@ context: node.js
 // include libraries and declare global variables:
 var express = require('express');	// include the express library
 var server = express();					  // create a server using express
+var bodyParser = require('body-parser'); // include body parser middleware
 var fs = require('fs');
 
 
 server.use('/',express.static('public'));   // set a static file directory
+server.use(bodyParser.urlencoded({extended: true})); // enable body parsing
 
 function postFile(request, response) {
-
+  console.log(request.body.digits);
   var fileName = __dirname + '/public/' + request.path;
   var data = fs.readFileSync(fileName);
   data = String(data);
   output = data.replace(/<temperature>/g, '25');
   output = output.replace(/<setpoint>/g, '18');
   output = output.replace(/<on>/g, 'on');
- res.writeHead(200, {'Content-Type': 'text/xml'});
+ response.writeHead(200, {'Content-Type': 'text/xml'});
   response.end(output);
 }
 
