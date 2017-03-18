c41949abba07148c5e657f299a6272fc3d4ec29a
tigoe <tom.igoe@gmail.com>
2017-03-18 16:23:28 -0400

    twilio additons

--- 3rd_edition/chapter10/catCamDeux/httpServer.js
+++ 3rd_edition/chapter10/catCamDeux/httpServer.js
@@ -5,8 +5,22 @@ context: node.js
 // include libraries and declare global variables:
 var express = require('express');	// include the express library
 var server = express();					  // create a server using express
+var fs = require('fs');
+
 
 server.use('/',express.static('public'));   // set a static file directory
 
+function postFile(request, response) {
+
+  var fileName = __dirname + '/public/' + request.path;
+  var data = fs.readFileSync(fileName);
+  data = String(data);
+  output = data.replace(/<temperature>/g, '25');
+  output = output.replace(/<setpoint>/g, '18');
+  output = output.replace(/<on>/g, 'on');
+  response.end(output);
+}
+
 // start the server:
 server.listen(8080);           // listen for HTTP
+server.post('/*', postFile);
