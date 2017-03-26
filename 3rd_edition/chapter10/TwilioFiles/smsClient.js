8fb2434fd5702cebb196c99623b2b4df76a1ba3b
tigoe <tom.igoe@gmail.com>
2017-03-26 15:17:28 -0400

    added Twilio SMS Client to chapter 10

---
+++ 3rd_edition/chapter10/TwilioFiles/smsClient.js
@@ -0,0 +1,50 @@
+/*
+  HTTPS client for Twilio SMS API
+  context: node.js
+*/
+
+var https = require('https');             // include https library
+var querystring = require('querystring'); // include querystring library
+var creds = require('./twilioCreds.js');  // your Twilio credentials
+
+var message = {         // the SMS message you plan to send
+  To:'+19177532699',
+  From:'+16467831513',
+  Body:'Hi There!'
+}
+// convert message to HTTP-style query string:
+var postData = querystring.stringify(message);
+
+// the HTTPS request options:
+var options = {
+  host: 'api.twilio.com',
+  auth: creds.apiKey + ':' + creds.auth,
+  port: 443,
+  path: '/2010-04-01/Accounts/' + creds.apiKey + '/Messages',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': postData.length
+  }
+};
+
+// request callback function:
+function confirm(response) {
+  var result = '';		// string to hold the response
+
+  function gather (data) {  // response 'data' callback function
+    result += data;
+  }
+
+  function printResult () { // response 'end' callback function
+    console.log(result);
+  }
+
+  response.on('data', gather);    // add each chunk to the result string
+  response.on('end',printResult); // print result when done
+}
+
+// make the actual request:
+var request = https.request(options, confirm);	// start it
+request.write(postData);							          // send the data
+request.end();												          // end it and send it
