255e14728aabddfcc9ea5c035e25bb39b78d09ea
tigoe <tom.igoe@gmail.com>
2017-04-26 06:36:36 -0400

    print sensor readings from accelerometer

--- 3rd_edition/chapter8/CurieIMUReader/CurieIMUReader.ino
+++ 3rd_edition/chapter8/CurieIMUReader/CurieIMUReader.ino
@@ -21,5 +21,17 @@ void loop() {
   // read and scale:
   CurieIMU.readAccelerometerScaled(x, y, z);
   CurieIMU.readGyroScaled(gx, gy, gz);
+  Serial.print("x: ");     // print x reading
+  Serial.print(x);
+  Serial.print(",\ty: ");  // print comma, tab, y reading
+  Serial.print(y);
+  Serial.print(",\tz: ");  // print comma, tab, z reading
+  Serial.print(z);
+  Serial.print("\tgx: ");  // print comma tab,  gx reading
+  Serial.print(gx);
+  Serial.print(",\tgy: "); // print comma, tab, gy reading
+  Serial.print(gy);
+  Serial.print(",\tgz: "); // print comma, tab, gz reading
+  Serial.println(gz);
 }
 
