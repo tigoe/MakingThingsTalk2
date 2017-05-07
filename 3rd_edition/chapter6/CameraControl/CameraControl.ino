e96b58ce3598889281475ea600c66002343a6dea
tigoe <tom.igoe@gmail.com>
2017-05-07 19:08:32 -0400

    added new camera control to chapter 6

--- 3rd_edition/chapter6/CameraControl/CameraControl.ino
+++ 3rd_edition/chapter6/CameraControl/CameraControl.ino
@@ -5,43 +5,59 @@
   This sketch controls a digital camera via an infrared LED.
 */
 
-// include the library for camera control:
-#include <multiCameraIrControl.h>
-
-const int pushButtonPin = 4;
-// set up in 3 to control the IR LED.
-// change this depending on the brand of your camera:
-Nikon camera(7);
-//Canon camera(7);
-
-// Variables will change:
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
+const int pushButtonPin = 4;      // pushbutton I/O pin
+const int IRPin = 7;              // IR LED I/O pin
+const int CAM_NIKON = 1;          // DSLR camera types
+const int CAM_CANON = 2;
+int buttonState = 0;              // current button state
+int lastButtonState = 0;          // previous button state
 
 void setup() {
-  Serial.begin(9600);
-  // initialize the pushButtonPin as input:
-  pinMode(pushButtonPin, INPUT);
+  pinMode(pushButtonPin, INPUT);  // initialize pusbutton
+  pinMode(IRPin, OUTPUT);         // initialize IR pin
 }
 
 void loop() {
   // read the pushButtonPin input pin:
   buttonState = digitalRead(pushButtonPin);
 
-  // compare the buttonState to its previous state
-  // if it's changed, and it's high now, then the person
-  // just pushed the button:
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
-      // send the signal to open the shutter:
-      camera.shutterNow();
-      Serial.println("click");
+  if (buttonState != lastButtonState) { // if button state changes,
+    if (buttonState == HIGH) {          // and button is pressed
+      shutterClick(CAM_CANON);          // send IR shutter signal
     }
   }
-  // save the current state as the last state,
-  //for next time through the loop
+  // save the current state for next comparison:
   lastButtonState = buttonState;
 }
 
-
-
+void shutterClick(int cameraType) {
+  if (cameraType == CAM_NIKON) {       // if it's a Nikon
+    unsigned long irFrequency = 38000; // IR freq. = 38K
+    for (int i = 0; i < 2; i++) {      // send message twice
+      IRPulse(2000, irFrequency);      // 2ms on
+      delay(27);                       // 27 ms off
+      IRPulse(400, irFrequency);       // 0.4ms on
+      delayMicroseconds(1500);         // 1.5ms off
+      IRPulse(500, irFrequency);       // 0.5ms on
+      delayMicroseconds(3500);         // 3.44ms off
+      IRPulse(500, irFrequency);       // 0.5ms on
+      delay(65);                       // 65ms delay before repeat
+    }
+  }
+  if (cameraType == CAM_CANON) {       // If it's a Canon
+    unsigned long irFrequency = 32700; // IR freq. = 32.7K
+    for (int i = 0; i < 2; i++) {      // send message twice
+      IRPulse(489, irFrequency);       // 16 pulses @ IR freq.
+      delayMicroseconds(7330);
+      IRPulse(489, irFrequency);       // 16 pulses @ IR freq.
+      delay(100);
+    }
+  }
+}
+// send an IR pulse at a frequency for a time:
+void IRPulse(unsigned long interval,  unsigned long frequency) {
+  unsigned long now = micros();     // get current time in micros
+  tone(IRPin, frequency);           // pulse IRPin at frequency
+  while (micros() - now < interval);// do nothing until interval passes
+  noTone(IRPin);                    // turn off pulse
+}
