7738dd02215269cff213de35074b46cfa97485d6
tigoe <tom.igoe@gmail.com>
2017-05-14 09:53:50 -0400

    added package.json files for chapter 7

--- 3rd_edition/chapter7/UDPServer/server.js
+++ 3rd_edition/chapter7/UDPServer/server.js
@@ -3,15 +3,15 @@
   context: node.js
 */
 
-// var express = require('express');           // include express
-// var server = express();					            // create a server using express
+var express = require('express');           // include express
+var server = express();					            // create a server using express
 var dgram = require('dgram');               // include datagram library
 var UDP_PORT = 8888;                        // the port on which to listen
 var udpServer = dgram.createSocket('udp4'); // create UDP socket
 var broadcastAddress = '192.168.0.255';     // broadcast address for this computer's subnet
 
-// serve static files from /public:
-// server.use('/',express.static('public'));
+serve static files from /public:
+server.use('/',express.static('public'));
 
 function udpBegin() {
   udpServer.setBroadcast(true);
@@ -22,12 +22,12 @@ function readMessage(message, sender) {
   console.log(sender.address + ':' + sender.port +' sent: ' + message);
 }
 
-// function serverResponse(request, response) {
-//   var message = request.params.message;
-//   var receiver = request.params.destination;
-//   udpServer.send(message, 0, data.length, UDP_PORT, receiver);
-//   response.end('Sent ' + message + ' to ' + receiver);
-// }
+function serverResponse(request, response) {
+  var message = request.params.message;
+  var receiver = request.params.destination;
+  udpServer.send(message, 0, data.length, UDP_PORT, receiver);
+  response.end('Sent ' + message + ' to ' + receiver);
+}
 
 udpServer.bind(UDP_PORT);             // bset the UDP port number
 udpServer.on('listening', udpBegin);  // start the UDP server
@@ -36,5 +36,5 @@ var data = "Hello";
 // send a broadcast message:
 udpServer.send(data, 0, data.length, UDP_PORT, broadcastAddress);
 
-// server.listen(8080);                    // start the HTTP server
-// server.get("/destination/:destination/msg/:message", serverResponse);
+server.listen(8080);                    // start the HTTP server
+server.get("/destination/:destination/msg/:message", serverResponse);
