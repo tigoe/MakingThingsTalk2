b6f8dee4ceae507d140a48958ba848026128149c
tigoe <tom.igoe@gmail.com>
2020-02-10 12:41:00 -0500

    Update TestHttpSSLClient.ino

--- 3rd_edition/chapter4/TestHttpSSLClient/TestHttpSSLClient.ino
+++ 3rd_edition/chapter4/TestHttpSSLClient/TestHttpSSLClient.ino
@@ -1,25 +1,28 @@
 /*
   Test HTTPS Client
-  To make this work you'll need to install the server's SSL certificate
-  using these instructions:
-  https://github.com/cvalenzuela/understanding-networks/blob/master/HTTPS.md
+  Based on the Test HTTP Client from Making Things Talk, 3rd ed.
 
+  If you need to install your server's SSL certificate, you can
+  use these instructions:
+  https://github.com/cvalenzuela/understanding-networks/blob/master/HTTPS.md
+  (works for both MKR1000 and MKR1010/Nano33IoT)
   created 12 Feb 2018
+  modified 8 Feb 2020
   by Tom Igoe
 */
 // include required libraries and config files
 #include <SPI.h>
-#include <WiFi101.h>
+
+#include <WiFiNINA.h>           // use this  for MKR1010/Nano 33 IoT modules
+//#include <WiFi101.h>          // use this  for MKR1000/WINC1500 modules
+//#include <ESP8266WiFi.h>      // use this for ESP8266 modules
 #include <ArduinoHttpClient.h>
 #include "arduino_secrets.h"
 
-// Set your network name (ssid) and password in the arduino_secrets.h tab
-const char ssid[] = SECRET_SSID;
-const char pass[] = SECRET_PASS;
-
-WiFiSSLClient netSocket;            // network socket to server
-const char server[] = "yourserver.com";  // server name
-String route = "/data";             // API route
+WiFiSSLClient netSocket;                  // network socket to server
+const char server[] = "www.example.com";  // server name
+String route = "/";                       // API route
+int portNumber = 443;
 
 void setup() {
   Serial.begin(9600);               // initialize serial communication
@@ -27,8 +30,8 @@ void setup() {
   // while you're not connected to a WiFi AP,
   while ( WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);           // print the network name (SSID)
-    WiFi.begin(ssid, pass);         // try to connect
+    Serial.println(SECRET_SSID);           // print the network name (SSID)
+    WiFi.begin(SECRET_SSID, SECRET_PASS);  // try to connect
     delay(2000);
   }
 
@@ -39,26 +42,16 @@ void setup() {
 }
 
 void loop() {
-  HttpClient https(netSocket, server, 443);      // make an HTTPS client
+  HttpClient http(netSocket, server, portNumber); // make an HTTP client
+  http.get(route);                                // make a GET request
 
-  int sensor = analogRead(A0);
-  String reading = "{\"sensor\"=" + String(sensor);
-  reading += "}";                         // close the JSON object
-
-  String contentType = "application/json";// Set the content type
-
-  https.beginRequest();                    // start assembling the request
-  https.post(route, contentType, reading );// make a post request
-  delay(10);                               // delay to wait for reply
-
-  while (https.connected()) {              // while connected to the server,
-    if (https.available()) {               // if there is a response from the server,
-      String result = https.readString();  // read it
-      Serial.print(result);                // and print it
+  while (http.connected()) {              // while connected to the server,
+    if (http.available()) {               // if there is a response from the server,
+      String result = http.readString();  // read it
+      Serial.print(result);               // and print it
     }
   }
   // when there's nothing left to the response,
-  https.stop();                     // close the request
+  http.stop();                     // close the request
   delay(10000);                    // wait 10 seconds
 }
-
