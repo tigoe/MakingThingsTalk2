181a7f7c9ab0d703221b2e0ba6da641ad3d01b6c
Tom Igoe <tom.igoe@gmail.com>
2020-11-02 17:09:04 -0500

    updated crypto.js example

--- 3rd_edition/chapter3/crypto/decrypt.js
+++ 3rd_edition/chapter3/crypto/decrypt.js
@@ -3,23 +3,36 @@ decryption script
 context: node.js
 to run this script add the filename and key at the end of your command like so:
 $ node encrypt.js message key
+
+Updated 2 Nov 2020 because createDecipher had been deprecated.
+
 */
 // include the crypto and filesystem libraries:
 var crypto = require('crypto');
 var fs = require('fs');
+// make a hash for the password:
+const hash = crypto.createHash('sha256');
+
 // take input from the command line:
 var key = process.argv[3];      // fourth element from the command line
 // set the location of the file that you'll output:
-var fileName = __dirname +"/"+ process.argv[2];
+var fileName = __dirname + "/" + process.argv[2];
 
-// create a decipherer:
-var decipher = crypto.createDecipher('aes-256-cbc', key);
+// hash the password from the command line input:
+hash.update(key);
 
 // callback function for the readFile function:
 function success(error, data) {
-  if (data){
+  if (data) {
+    // get the data from the file:
     data = data.toString();
-    var decryptedMsg = decipher.update(data, 'hex', 'utf8');
+    // split it on the -:
+    var results = data.split('-');
+    // create a decipherer:
+    var iv = Buffer.from(results[1], 'hex');
+    var decipher = crypto.createDecipheriv('aes-256-cbc', hash.digest(), iv);
+// decrypt the first part of the data:
+    var decryptedMsg = decipher.update(results[0], 'hex', 'utf8');
     decryptedMsg += decipher.final('utf8');
     console.log('I read this from the file: ' + decryptedMsg);
   } else if (error) {
--- 3rd_edition/chapter3/crypto/encrypt.js
+++ 3rd_edition/chapter3/crypto/encrypt.js
@@ -3,23 +3,38 @@ encryption script
 context: node.js
 to run this script add the message and key at the end of your command like so:
 $ node encrypt.js message key
+
+Updated 2 Nov 2020 because createCipher had been deprecated.
+
 */
 // include the crypto and filesystem libraries:
 var crypto = require('crypto');
 var fs = require('fs');
+// make a hash for the password:
+const hash = crypto.createHash('sha256');
+
 // take input from the command line:
 var key = process.argv[3];      // fourth argument from the command line
-var message = process.argv[2];  // third argument from the command line
+var message =  process.argv[2];  // third argument from the command line
+
+// hash the password from the command line input:
+hash.update(key);
 
 // set the location of the file that you'll output
 // (whatever directory you call this script from ):
 var fileName = 'info.txt';
 
 // create a cipher:
-var cipher = crypto.createCipher('aes-256-cbc', key);
+const iv = crypto.randomBytes(16);
+var cipher = crypto.createCipheriv('aes-256-cbc', hash.digest(), iv);
+console.log(iv.toString('hex'));
+
 // add the message to the cipher and finalize it:
 var encryptedMsg =  cipher.update(message, 'utf8', 'hex');
-encryptedMsg += cipher.final('hex')
+encryptedMsg += cipher.final('hex');
+// add a - and then the initialization vector for the cipher, hex-encoded:
+encryptedMsg += '-';
+encryptedMsg += iv.toString('hex');
 
 // callback function for the writeFile function:
 function success(data) {
